8c7d78a6e4a97bf9a89c7c8c5d3cc85792fcb776
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-03-30 15:15:14 -0400

    restricted page

---
+++ restricted.php
@@ -0,0 +1,11 @@
+<?php require_once 'includes/header.php' ?>
+
+<body>
+    <main class="container restricted text-center">
+        <h2 class="py-5"> Sorry, you must be logged into view this content! </h2>
+        <a href="index.php" class="btn btn-secondary mb-5"> Back to Home Page </a>
+    </main>
+</body>
+
+</html>
+<?php require_once 'includes/footer.php' ?>
